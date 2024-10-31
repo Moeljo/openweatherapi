6437874ee4d01e516386789a6629962f912ead50
Ricardo <ricardomoeljoredjo@hotmail.com>
2024-10-31 10:49:32 +0100

    Added 3 testcases

--- tests/api.spec.ts
+++ tests/api.spec.ts
@@ -2,24 +2,68 @@ import { test, expect } from '@playwright/test';
 
 const API_KEY = '0fiuZFh4'
 
-test.describe.parallel("Collection Retrieval", () => {
-    test("Retrieve collection and check title ", async ({ request }) => {
+
+test.describe.parallel("Successful response", () => {
+    test("Validate response status 200", async ({ request }) => {
         const response = await request.get(`?key=${API_KEY}`)
         expect(response.status()).toBe(200)
-        const data = await response.json()//voorkomen dat aschryone proces door gaat
-        expect(data.artObjects.length).toBeGreaterThan(0)//wil weten of er iets in de array artObjects zit 
+    })
+})
+
+test.describe.parallel("Collection Retrieval", () => {
+    test("Retrieve collection and check for title ", async ({ request }) => {
+        const response = await request.get(`?key=${API_KEY}`)
+        expect(response.status()).toBe(200)
+        const data = await response.json()
+        expect(data.artObjects.length).toBeGreaterThan(0)
         expect(data.artObjects[0]).toHaveProperty('title')
     })
 })
 
-test.describe.parallel("Collection Detail Retrieval", () => {
+test.describe.parallel("Collection Retrieval", () => {
+    test("Retrieve collection and validate title ", async ({ request }) => {
+        const response = await request.get(`?key=${API_KEY}`)
+        expect(response.status()).toBe(200)
+        const data = await response.json()
+        expect(data.artObjects.length).toBeGreaterThan(0) 
+        expect(data.artObjects[0]).toHaveProperty('title')
+        expect(data.artObjects[0].title).toBe('Blauwe papegaai')
+    })
+})
+
+test.describe.parallel("Collection Retrieval Details", () => {
+    test("Retrieve collection and check objectnumber & longtitle ", async ({ request }) => {
+        const response = await request.get(`?key=${API_KEY}`)
+        expect(response.status()).toBe(200)
+        const data = await response.json()
+        expect(data.artObjects.length).toBeGreaterThan(0)
+        expect(data.artObjects[0]).toHaveProperty('objectNumber')
+        expect(data.artObjects[0].objectNumber).toBe('BK-17496')
+        expect(data.artObjects[0]).toHaveProperty('longTitle')
+        expect(data.artObjects[0].longTitle).toBe('Blauwe papegaai, Meissener Porzellan Manufaktur, 1731')
+    })
+})
+
+test.describe.parallel("Collection Retrieval Details", () => {
+    test("Fail this test for reporting", async ({ request }) => {
+        const objectNumber = 'SK-A-447' 
+        const response = await request.get(`?objectNumber=${objectNumber}&key=${API_KEY}`)
+        expect(response.status()).toBe(200)
+        const data = await response.json()
+        expect(data.artObjects[0]).toHaveProperty('title')
+        expect(data.artObjects[0]).toHaveProperty('principalOrFirstMaker')
+        expect(data.artObjects[0].principalOrFirstMaker).toBe('Does not exist') // must be Meissener Porzellan Manufaktur
+    })
+})
+
+test.describe.parallel("Collection Retrieval Details", () => {
     test("Retrieve Collection and check details", async ({ request }) => {
-        const objectNumber = 'RP-P-OB-184' //Hier zit geen limiet op?
+        const objectNumber = 'RP-P-OB-184' 
         const response = await request.get(`?objectNumber=${objectNumber}&key=${API_KEY}`)
         expect(response.status()).toBe(200)
         const data = await response.json()
         expect(data.artObjects[1]).toHaveProperty('title')
         expect(data.artObjects[0]).toHaveProperty('principalOrFirstMaker')
-        expect(data.artObjects[0].principalOrFirstMaker).toBe('anoniem')//tobe Willem Claesz. Heda is veranderd?
+        expect(data.artObjects[0].principalOrFirstMaker).toBe('Meissener Porzellan Manufaktur')
     })
 })
